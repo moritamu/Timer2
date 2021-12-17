553641020c5db865c2faf01a059a33b68971947c
moritamu <moris@r.sannet.ne.jp>
2021-12-18 05:39:26 +0900

    Initial Commit

---
+++ Timer2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer2
+//
+//  Created by 森田晋 on 2021/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer2/Timer2App.swift
@@ -0,0 +1,17 @@
+//
+//  Timer2App.swift
+//  Timer2
+//
+//  Created by 森田晋 on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct Timer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
