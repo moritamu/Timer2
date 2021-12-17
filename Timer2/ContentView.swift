1497bea869e9b711e35173ce2172bb64e0b77fdd
moritamu <moris@r.sannet.ne.jp>
2021-12-18 06:24:33 +0900

    stopできた

--- Timer2/ContentView.swift
+++ Timer2/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var timer:Timer?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(count) 秒")
+                .padding()
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                //scheduledTimer()メソッドを使用
+                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                    self.count += 1
+                    if count == 5 {
+                        timer?.invalidate()
+                        count = 0
+                    }
+                }
+            }
+        }
     }
 }
 
