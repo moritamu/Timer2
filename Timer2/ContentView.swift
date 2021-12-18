004776354b3f14bb1f53eaeac283dd1f894b55d1
moritamu <moris@r.sannet.ne.jp>
2021-12-18 10:42:17 +0900

    Soundをたした

--- Timer2/ContentView.swift
+++ Timer2/ContentView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct ContentView: View {
     @State var count = 0
     @State var timer:Timer?
+    let soundPlayer = SoundPlayer()
     
     var body: some View {
         VStack {
             Text("\(count) 秒")
                 .padding()
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            Button("スタート") {
                 //scheduledTimer()メソッドを使用
                 timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
                     self.count += 1
                     if count == 5 {
                         timer?.invalidate()
                         count = 0
+                        soundPlayer.cymbalPlay()
                     }
                 }
             }
